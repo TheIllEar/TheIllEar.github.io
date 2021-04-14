8dd410dd53b3941eb0b3d8c8db9567620b3fc36a
TheIllEar <theillear@gmail.com>
2021-04-14 23:32:23 +0300

    Rework 2stk

--- SiteService/script/mail.js
+++ SiteService/script/mail.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
 			url: "mail.php", //Change
 			data: th.serialize()
 		}).done(function() {
-			alert("Thank you!");
+			alert(«Спасибо! Наш менеджер скоро свяжется с Вами)»);
 			setTimeout(function() {
 				// Done Functions
 				th.trigger("reset");
